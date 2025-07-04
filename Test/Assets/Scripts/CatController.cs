c920361f4d3c076d236b9bb9dbbeb40afc3020af
Calcoa <112841042+CalcoaMara@users.noreply.github.com>
2025-07-04 10:07:40 -0700

    added cat asset
    Added cat asset

---
+++ Test/Assets/Scripts/CatController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    [SerializeField] private SphereCollider headTrigger;
+    [SerializeField] private CapsuleCollider torsoTrigger;
+    [SerializeField] private CapsuleCollider grounder;
+
+}
