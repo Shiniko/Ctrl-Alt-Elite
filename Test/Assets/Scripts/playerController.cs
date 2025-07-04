098aca71ea11ebb16ebc48c333cc0c26a0ee2967
Shiniko <jdotlabs@gmail.com>
2025-07-03 22:16:26 -0700

    Tester

---
+++ Test/Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public class playerController : MonoBehaviour {
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay() {
+        isGrounded = true;
+    }
+        
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
