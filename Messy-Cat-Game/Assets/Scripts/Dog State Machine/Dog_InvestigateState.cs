95c9e84cb37c8a4e9084d714614a1e3b45252981
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-07-14 13:14:39 -0700

    Update Dog_InvestigateState.cs

--- Messy-Cat-Game/Assets/Scripts/Dog State Machine/Dog_InvestigateState.cs
+++ Messy-Cat-Game/Assets/Scripts/Dog State Machine/Dog_InvestigateState.cs
@@ -1,4 +1,25 @@
-Public class Dog_InvestigateState 
-{
+using UnityEngine;
 
+public class Dog_InvestigateState : StateMachineBehaviour
+{
+// The suspicious Event the dog will investigate
+SuspiciousEvent suspiciousEvent;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
 }
