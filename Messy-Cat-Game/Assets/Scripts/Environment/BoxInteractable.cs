e65525c9725e864a39ed99ccc5439d92334815d0
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-07-20 13:42:07 -0700

    Removed Box Interactable
    No longer needed, it was just a test script.
    Also deleted SampleScene as that was just a test for the interactable scripts

--- Messy-Cat-Game/Assets/Scripts/Environment/BoxInteractable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-[RequireComponent(typeof(Collider))]
-public class BoxInteractable : MonoBehaviour, IInteractable
-{
-    public void Interact()
-    {
-        Debug.Log("Interacted with!!!");
-    }
-}
