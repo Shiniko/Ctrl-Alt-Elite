4d7e69e92d763a89d678b4d64043fe956d571a10
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-07-19 20:33:04 -0700

    Created Stall State

---
+++ Messy-Cat-Game/Assets/Dog_Stalling.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dog_Stalling : StateMachineBehaviour
+{
+    DogContext _dogContext;
+    private bool _stalling;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _dogContext = animator.GetComponent<DogContext>();
+        _stalling = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(_stalling == false)
+        {
+            _dogContext.StartCoroutine(Stall());
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    IEnumerator Stall()
+    {
+        _stalling = true;
+        yield return new WaitForSeconds(_dogContext.GetStallTime());
+        _dogContext.GetComponent<Animator>().SetBool("Stalling",false);
+    }
+}
